84440b6e61d001148880e75b5af7db81669f3520
cortelucas <corte1994.lc@gmail.com>
2022-08-04 11:03:45 -0300

    feat: learn type boolean

---
+++ module-02/02-type-boolean/typeBoolean.js
@@ -0,0 +1,21 @@
+/**
+ * arquivo: booleanType.ts
+ * descrição: arquivo responsável por ensinar conceitos básicos sobre 'Tipo Boolean'
+ * data: 04/08/2022
+ * author: Lucas Corte <@cortelucas>
+ * doc referência: https://www.typescriptlang.org/docs/handbook/basic-types.html#boolean
+ * Transpilação do arquivo: <Windows> CTRL + SHIFT + B -> tsc: build/watch
+ */
+// ==> Exemplo 01
+var taskCompleted = true;
+var taskOpen = false;
+console.log(taskCompleted);
+console.log(taskOpen);
+// ==> Exemplo 02
+var complete = true;
+if (!complete) {
+    console.log('Tarefa foi concluída com sucesso!');
+}
+else {
+    console.log('Tarefa Pendente!');
+}
